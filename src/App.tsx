9d1df9e9c79d9c7a4e87598a8aa40628f5160b36
Wren Hawthorne <wrenhawth@gmail.com>
2026-03-31 00:30:50 -0700

    Added basic synth chords

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,74 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+import { MonoSynth, now, PolySynth } from "tone";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const polySynth = useRef<PolySynth | null>(null);
+
+  useEffect(() => {
+    polySynth.current = new PolySynth({
+      voice: MonoSynth,
+      maxPolyphony: 14,
+    }).toDestination();
+  });
 
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>{" "}
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+          <h1>UbiChord</h1>
+          <button
+            onClick={() => {
+              polySynth?.current?.releaseAll("8n");
+              polySynth?.current?.triggerAttackRelease(
+                ["C4", "E4", "G4"],
+                "2n",
+                now(),
+              );
+            }}
+          >
+            C Major
+          </button>
 
-      <div className="ticks"></div>
+          <button
+            onClick={() => {
+              polySynth?.current?.releaseAll();
+              polySynth?.current?.triggerAttackRelease(
+                ["A3", "C4", "E4"],
+                "2n",
+                now(),
+              );
+            }}
+          >
+            A minor
+          </button>
+          <button
+            onClick={() => {
+              polySynth?.current?.releaseAll();
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+              polySynth?.current?.triggerAttackRelease(
+                ["F4", "A4", "C5"],
+                "2n",
+                now(),
+              );
+            }}
+          >
+            F Major
+          </button>
+          <button
+            onClick={() => {
+              polySynth?.current?.releaseAll();
+
+              polySynth?.current?.triggerAttackRelease(
+                ["G4", "B4", "D5"],
+                "2n",
+                now(),
+              );
+            }}
+          >
+            G Major
+          </button>
         </div>
       </section>
 
