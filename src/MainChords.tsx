b9a82453cfb4f6300b3af4ba89a23398a24fa9d8
ldflores <32505357+ldflores@users.noreply.github.com>
2026-04-11 01:09:15 -0700

    Add hand icon, update styles, add description

--- src/MainChords.tsx
+++ src/MainChords.tsx
@@ -1,3 +1,5 @@
+import HandIcon from './assets/hand-index-thumb.svg?react';
+
 export default function MainChords({
   playChord,
 }: {
@@ -5,14 +7,21 @@ export default function MainChords({
 }) {
   return (
     <>
+      <div className="orbit-0">
+        <div className="satellite grow-1x">
+          <div className="capsule">
+            <HandIcon className="hand-icon" width="32" height="32"/>
+          </div>
+        </div>
+      </div>
       <div className="orbit-3">
         {/* <button>C</button> */}
-        <o-arc className="red grow-2x" onClick={() => playChord("C")} />
-        <o-arc className="orange grow-2x" onClick={() => playChord("Dm")} />
-        <o-arc className="yellow grow-2x" onClick={() => playChord("Em")} />
-        <o-arc className="green grow-2x" onClick={() => playChord("F")} />
-        <o-arc className="blue grow-2x" onClick={() => playChord("G")} />
-        <o-arc className="indigo grow-2x" onClick={() => playChord("Am")} />
+        <o-arc className="red grow-2x gap-8" shape="rounded" onClick={() => playChord("C")} />
+        <o-arc className="orange grow-2x gap-8" shape="rounded" onClick={() => playChord("Dm")} />
+        <o-arc className="yellow grow-2x gap-8" shape="rounded" onClick={() => playChord("Em")} />
+        <o-arc className="green grow-2x gap-8" shape="rounded" onClick={() => playChord("F")} />
+        <o-arc className="blue grow-2x gap-8" shape="rounded" onClick={() => playChord("G")} />
+        <o-arc className="indigo grow-2x gap-8" shape="rounded" onClick={() => playChord("Am")} />
       </div>
       <div className="orbit-3 from-25 labels">
         <div className="satellite">
